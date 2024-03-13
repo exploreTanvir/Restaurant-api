f4d3809ae1b313321af757632a44cc12520402c6
Tanvir_Hossen <tanvirhossen853@gmail.com>
2024-03-13 15:38:56 +0600

    Restaurant API

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import React from 'react'
 import "./App.css"
 import Home from './components/Home'
 import About from './components/About'
+import Work from './components/Work'
+import Testimonial from './components/Testimonial'
+import Footer from './components/Footer'
 function App() {
   return (
     <div>
       <Home/>
       <About/>
+      <Work/>
+      <Testimonial/>
+      <Footer/>
     </div>
   )
 }
